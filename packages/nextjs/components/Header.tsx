ef6f6b6d5abccabaaef3ff1261c2bf4f7505b4e6
WebMarpa <mario_amilcar_parodi@hotmail.com>
2024-12-17 05:02:46 +0000

    Adding Page Swap, Pool, Mint

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, CurrencyDollarIcon, ArrowsRightLeftIcon, TagIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -20,6 +20,21 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
 
+  {
+    label: "Swap",
+    href: "/Swap",
+    icon: <ArrowsRightLeftIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Pool",
+    href: "/Pool",
+    icon: <CurrencyDollarIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Mint",
+    href: "/Mint",
+    icon: <TagIcon className="h-4 w-4" />,
+  },
   {
     label: "Debug Contracts",
     href: "/debug",
@@ -95,7 +110,7 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="text-xs">By Mario Parodi</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
